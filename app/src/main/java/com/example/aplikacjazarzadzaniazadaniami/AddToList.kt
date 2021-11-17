6287728ba91dacabcb8953f96e13020c1c0831c3
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2021-11-17 17:57:47 +0100

    Scroll + hidesoft
    Git Gud

--- app/src/main/java/com/example/aplikacjazarzadzaniazadaniami/AddToList.kt
+++ app/src/main/java/com/example/aplikacjazarzadzaniazadaniami/AddToList.kt
@@ -1,5 +1,6 @@
 package com.example.aplikacjazarzadzaniazadaniami
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,22 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.aplikacjazarzadzaniazadaniami.databinding.ListAddBinding
 import android.widget.Toast
+import android.view.MotionEvent
+
+import android.view.View.OnTouchListener
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import android.app.Activity
+
+
+
+
+
+
+
+
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -29,14 +46,37 @@ class AddToList : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.descAdd.movementMethod
 
         binding.add.setOnClickListener{
             Toast.makeText(context, "Dodano zadanie!", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
 //            Log.d("Beniz", "hehe");
         }
+
+        binding.descAdd.setOnTouchListener(OnTouchListener { v, event ->
+            if (binding.descAdd.hasFocus()) {
+                v.parent.requestDisallowInterceptTouchEvent(true)
+                when (event.action and MotionEvent.ACTION_MASK) {
+                    MotionEvent.ACTION_SCROLL -> {
+                        v.parent.requestDisallowInterceptTouchEvent(false)
+                        return@OnTouchListener true
+                    }
+                }
+            }
+            false
+        })
+
+        binding.scrollAdd.setOnTouchListener(OnTouchListener { v, event ->
+                val imm =
+                    requireContext()!!.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+                false
+            })
     }
 
     override fun onDestroyView() {
