9f6dc53e8f13c26dc421750e6b3fadbacdbe012f
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2021-12-02 15:02:43 +0100

    Zakupy coś tam
    hehe

--- app/src/main/java/com/example/aplikacjazarzadzaniazadaniami/Zakupy.kt
+++ app/src/main/java/com/example/aplikacjazarzadzaniazadaniami/Zakupy.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.aplikacjazarzadzaniazadaniami.databinding.ZakupyBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+import java.io.FileReader
 
 
 class Zakupy : Fragment() {
@@ -16,8 +22,8 @@ class Zakupy : Fragment() {
     private var _binding: ZakupyBinding? = null
     private val binding get() = _binding!!
 
-//    private val list = generateDummyList(10)
-//    private val adapter = AdapterZakupy(list)
+    private val list = generateDummyList(10)
+    private val adapter = AdapterZakupy(list)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -26,70 +32,93 @@ class Zakupy : Fragment() {
 
         _binding = ZakupyBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//            binding.rec1.adapter = adapter
-        binding.rec1.layoutManager = LinearLayoutManager(this.context)
-        binding.rec1.setHasFixedSize(true)
+        val path = context?.getExternalFilesDir(null)
+        val letDirectory = File(path, "LET")
+        if(File(letDirectory,"Zakupy.json").exists()) {
+            val jsonArray : MutableList<ZakupyClass> = Gson().fromJson(FileReader(File(letDirectory,"Zakupy.json")), object : TypeToken<MutableList<ZakupyClass>>(){}.type)
+
+
+            val list = generateDummyList(jsonArray.size)
+//            Log.d("hehe", "hehe: ")
+            binding.rec1.adapter = AdapterZakupy(list)
+            binding.rec1.layoutManager = LinearLayoutManager(this.context)
+            binding.rec1.setHasFixedSize(true)
+        }
 
         binding.fab1.setOnClickListener{
 
             val builder = AlertDialog.Builder(this.context,R.style.AlertDialogCustom)
                 .create()
             val view = layoutInflater.inflate(R.layout.custom_dialog,null)
-            val  button = view.findViewById<Button>(R.id.dialogDismiss_button)
+            val przedmiot: EditText = view.findViewById(R.id.itemname)
+            val button = view.findViewById<Button>(R.id.dialogDismiss_button)
+            val buttonacc = view.findViewById<Button>(R.id.dialogAccept_button)
             builder.setView(view)
             button.setOnClickListener {
                 builder.dismiss()
             }
+            buttonacc.setOnClickListener{
+                if(przedmiot.text.toString().trim() != "") {
+                    val path = context?.getExternalFilesDir(null)
+
+                    val letDirectory = File(path, "LET")
+                    letDirectory.mkdirs()
+                    val file = File(letDirectory, "Zakupy.json")
+
+                    val zakup = ZakupyClass()
+
+                    zakup.id = 1
+
+                    zakup.item = przedmiot.text.toString()
+//                    Log.d("Hehe", przedmiot.text.toString())
+                    zakup.itemPurchase = true
+
+                    if(!file.exists()) {
+                        val jsonArray: MutableList<ZakupyClass> = mutableListOf()
+                        jsonArray.add(zakup)
+                        val jsonString = Gson().toJson(jsonArray)
+                        file.appendText(jsonString)
+                    }else{
+                        val gson = Gson()
+                        val jsonArray : MutableList<ZakupyClass> = gson.fromJson(FileReader(file), object : TypeToken<MutableList<ZakupyClass>>(){}.type)
+                        jsonArray.add(zakup)
+                        val jsonString = Gson().toJson(jsonArray)
+                        file.writeText(jsonString)
+                    }
+                }else{
+                    Toast.makeText(context, "Nie podano nazwy przedmiotu!", Toast.LENGTH_SHORT).show()
+                }
+            }
             builder.setCanceledOnTouchOutside(false)
             builder.show()
-
-//            alert.setTitle("Podaj przedmiot")
-//            val input = EditText(this.context)
-//            input.hint = "Przedmiot"
-//            input.inputType = InputType.TYPE_CLASS_TEXT
-//            alert.setView(input)
-//
-//            alert.setPositiveButton(
-//                "Dodaj",
-//                DialogInterface.OnClickListener { dialog, whichButton ->
-//                    //Your action here
-//                })
-//
-//            alert.setNegativeButton("Anuluj",
-//                DialogInterface.OnClickListener { dialog, whichButton -> })
-//
-//            alert.show()
-//                val index = 0;
-//
-//                val newItem = CardViewZakupy(
-//                    "Hehe",
-//                    true
-//                )
-//                list.add(index, newItem)
-//                adapter.notifyItemInserted(index)
         }
     }
 
 
-//    private fun generateDummyList(size: Int): ArrayList<CardViewZakupy>{
-//        val list = ArrayList<CardViewZakupy>()
-//
-//        for(i in 0 until size) {
-//            val item = CardViewZakupy(
-//                text1 = "hehe",
-//                checkbox = true
-//            )
-//            list += item
-//        }
-//                count++
-//        return list
-//    }
+    private fun generateDummyList(size: Int): List<CardViewZakupy>{
+        val list = ArrayList<CardViewZakupy>()
+
+        val path = context?.getExternalFilesDir(null)
+        val letDirectory = File(path, "LET")
+        var count = 0
+        if(File(letDirectory,"Zakupy.json").exists()) {
+            val jsonArray : MutableList<ZakupyClass> = Gson().fromJson(FileReader(File(letDirectory,"Zakupy.json")), object : TypeToken<MutableList<ZakupyClass>>(){}.type)
+            while(count < jsonArray.size){
+                val item = CardViewZakupy(
+                    jsonArray[count].item.toString(),
+                    true
+                )
+                list += item
+                count++
+            }
+        }
+        return list
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
---
+++ app/src/main/java/com/example/aplikacjazarzadzaniazadaniami/ZakupyClass.kt
@@ -0,0 +1,8 @@
+package com.example.aplikacjazarzadzaniazadaniami
+
+class ZakupyClass {
+
+    var id: Int? = null
+    var item: String? = null
+    var itemPurchase: Boolean? = null
+}
